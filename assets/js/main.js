ff159be8ff9161ff97899719195bfa3f81615079
Alvaro Fernandez <alvaro.fernandezqui@mail.com>
2022-12-01 09:05:53 -0300

    Primer commit

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+
+(function($){
+
+    $(document).ready(
+    
+      function(){
+    
+        // Comprobar si estamos, al menos, 100 px por debajo de la posición top
+        // para mostrar o esconder el botón
+        $(window).scroll(function(){
+    
+          if ( $(this).scrollTop() > 90 ) {
+    
+            $('.scroll-top-btn').fadeIn();
+    
+          } else {
+    
+            $('.scroll-top-btn').fadeOut();
+    
+          }
+    
+        });
+    
+        // al hacer click, animar el scroll hacia arriba
+        $('.scroll-top-btn').click( function( e ) {
+    
+          e.preventDefault();
+          $('html, body').animate( {scrollTop : 0}, 800 );
+    
+        });
+    
+      });
+    
+    })(jQuery);
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
